5a593f09c6967822e80a04d47a2397dc5cc78bbc
JUAN manuel calleros <116044083+manuelcalso@users.noreply.github.com>
2023-12-06 14:05:30 -0600

    se actualiza db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,11 +4,14 @@ dotenv.config();
 
 //LOCAL = mongodb://localhost:27017/ecommerce
 //PRODUCCION = mongodb+srv://juancallerossol:<password>@pizzeriadb.uqyyig4.mongodb.net/ecommerce
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.BASE_URL_DB_PRODUCTION, {});
-    console.log("base de datos conectada");
+    await mongoose.connect(process.env.BASE_URL_DB_PRODUCTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Base de datos conectada.");
   } catch (error) {
     console.log(error);
     process.exit(1);
